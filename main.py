0653ce8178ffa6d77bc5d561cd26a22551897004
Shagun-2803 <contactshagunchaudhari@gmail.com>
2026-05-25 17:25:58 +0530

    added a category for url

--- main.py
+++ main.py
@@ -6,13 +6,54 @@ import os
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
 
-url = f"https://newsapi.org/v2/top-headlines?country=us&apiKey={API_KEY}"
+categories = [
+    "business",
+    "entertainment",
+    "general",
+    "health",
+    "science",
+    "sports",
+    "technology"
+]
 
-response = requests.get(url)
-data = response.json()
+def fetch_news(category):
+  url = f"https://newsapi.org/v2/top-headlines?country=us&category={category}&apiKey={API_KEY}"
+  response = requests.get(url)
+  data = response.json()
+  return data
 
-for index,article in enumerate(data["articles"] , start= 1):
-  print(f"{index}: {article.get('title')}")
-  print(f"--> Author Name: {article.get('author')}")
-  print(f"--> Source Name: {article.get('source', {}).get('name')}")
-print(response)
+def is_valid_category(category):
+  if category in categories:
+    return True
+  else:
+    return False
+
+def display_news(data):
+  for index,article in enumerate(data["articles"] , start= 1):
+    print(f"{index}: {article.get('title')}")
+    print(f"--> Author Name: {article.get('author')}")
+    print(f"--> Source Name: {article.get('source', {}).get('name')}")
+
+
+
+def main():
+  
+  while True:
+    input_category = input("Enter required category: ").lower()
+    if is_valid_category(input_category):
+      data = fetch_news(input_category)
+      display_news(data)
+      break
+
+    else:
+      print("Enter Valid Category!")
+
+main()
+
+
+
+
+
+
+
+
