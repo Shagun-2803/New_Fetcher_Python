50fa0c674b50ab7018d51f7cadc346e090823f95
Shagun-2803 <contactshagunchaudhari@gmail.com>
2026-05-25 17:34:55 +0530

    modified a bit

--- main.py
+++ main.py
@@ -17,11 +17,11 @@ categories = [
 ]
 
 def fetch_news(category):
-  url = f"https://newsapi.org/v2/top-headlines?country=us&category={category}&apiKey={API_KEY}"
-  response = requests.get(url)
-  data = response.json()
-  return data
-
+    url = f"https://newsapi.org/v2/top-headlines?country=us&category={category}&apiKey={API_KEY}"
+    response = requests.get(url)
+    data = response.json()
+    return data
+  
 def is_valid_category(category):
   if category in categories:
     return True
@@ -33,12 +33,17 @@ def display_news(data):
     print(f"{index}: {article.get('title')}")
     print(f"--> Author Name: {article.get('author')}")
     print(f"--> Source Name: {article.get('source', {}).get('name')}")
+    print("-"*50)
 
 
 
 def main():
   
   while True:
+    print("Available Categories: ")
+    for category in categories:
+      print(category)
+
     input_category = input("Enter required category: ").lower()
     if is_valid_category(input_category):
       data = fetch_news(input_category)
