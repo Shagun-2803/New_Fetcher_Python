5ab8103186fcdf3c3a2c4f4bd4b99f055f9d8c6d
Shagun-2803 <contactshagunchaudhari@gmail.com>
2026-05-26 22:19:18 +0530

    converted into classes and objects

--- main.py
+++ main.py
@@ -4,50 +4,55 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-API_KEY = os.getenv("API_KEY")
 
-categories = [
-    "business",
-    "entertainment",
-    "general",
-    "health",
-    "science",
-    "sports",
-    "technology"
-]
+class newsFetcher:
+  def __init__(self):
+    self.categories = [
+      "business",
+      "entertainment",
+      "general",
+      "health",
+      "science",
+      "sports",
+      "technology"
+    ]
 
-def fetch_news(category):
-    url = f"https://newsapi.org/v2/top-headlines?country=us&category={category}&apiKey={API_KEY}"
-    response = requests.get(url)
-    data = response.json()
-    return data
-  
-def is_valid_category(category):
-  if category in categories:
-    return True
-  else:
-    return False
-
-def display_news(data):
-  for index,article in enumerate(data["articles"] , start= 1):
-    print(f"{index}: {article.get('title')}")
-    print(f"--> Author Name: {article.get('author')}")
-    print(f"--> Source Name: {article.get('source', {}).get('name')}")
-    print("-"*50)
+    self.API_KEY = os.getenv("API_KEY")
 
 
+  def fetch_news(self,category):
+      url = f"https://newsapi.org/v2/top-headlines?country=us&category={category}&apiKey={self.API_KEY}"
+      response = requests.get(url)
+      data = response.json()
+      return data
+    
+  def is_valid_category(self,category):
+    if category in self.categories:
+      return True
+    else:
+      return False
+
+  def display_news(self,data):
+    for index,article in enumerate(data["articles"] , start= 1):
+      print(f"{index}: {article.get('title')}")
+      print(f"--> Author Name: {article.get('author')}")
+      print(f"--> Source Name: {article.get('source', {}).get('name')}")
+      print("-"*50)
+
+
+newsapp= newsFetcher()
 
 def main():
   
   while True:
     print("Available Categories: ")
-    for category in categories:
+    for category in newsapp.categories:
       print(category)
 
     input_category = input("Enter required category: ").lower()
-    if is_valid_category(input_category):
-      data = fetch_news(input_category)
-      display_news(data)
+    if newsapp.is_valid_category(input_category):
+      data = newsapp.fetch_news(input_category)
+      newsapp.display_news(data)
       break
 
     else:
